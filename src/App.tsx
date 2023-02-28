09ae4b2c325d72731931ad84ba2c18a3589031d2
Kaung Shein <nicholasling1412@gmail.com>
2023-02-28 06:54:07 -0800

    Add nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './components/MediaListWrapper/MediaListWrapper';
 import MediaListWrapper from './components/MediaListWrapper/MediaListWrapper';
+import Nav from './components/Nav/Nav';
 
 function App() {
   const query_client = new QueryClient({
@@ -12,13 +13,19 @@ function App() {
       },
     },
   });
-  const [is_movies_only, setMoviesOnly] = useState(false);
+  const [is_movies_only, setIsMoviesOnly] = useState(false);
+  const [is_about_open, setIsAboutOpen] = useState(false);
 
   return (
     <QueryClientProvider client={query_client}>
-      <div className="App">
+      <main className="App">
+        <Nav
+          is_movies_only={is_movies_only}
+          setIsMoviesOnly={setIsMoviesOnly}
+          setIsAboutOpen={setIsAboutOpen}
+        />
         <MediaListWrapper is_movies_only={is_movies_only} />
-      </div>
+      </main>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,29 @@
+import styles from './Nav.module.scss';
+
+type PropTypes = {
+  is_movies_only: boolean;
+  setIsMoviesOnly: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsAboutOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function Nav({ is_movies_only, setIsMoviesOnly, setIsAboutOpen }: PropTypes) {
+  return (
+    <nav>
+      <button onClick={() => setIsAboutOpen(true)}>About Site</button>
+      <div className={styles.gif_container}>
+        <img
+          src="https://media.giphy.com/media/XmppNRlrlu2SA/giphy.gif"
+          alt="futuristic blob looking thing animated"
+        />
+      </div>
+      <button
+        className={is_movies_only ? styles.active : ''}
+        onClick={() => {
+          is_movies_only ? setIsMoviesOnly(false) : setIsMoviesOnly(true);
+        }}
+      >
+        Movies Only
+      </button>
+    </nav>
+  );
+}
