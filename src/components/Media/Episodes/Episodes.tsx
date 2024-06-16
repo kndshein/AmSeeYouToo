081a4eba32419d5bd8dfa65b35675aee81354044
kndshein <kndshein@gmail.com>
2024-06-16 15:47:55 -0700

    Animate Episodes

--- src/components/Media/Episodes/Episodes.tsx
+++ src/components/Media/Episodes/Episodes.tsx
@@ -1,6 +1,7 @@
 import styles from './Episodes.module.scss';
 import { TmdbType } from '../../../types/Tmdb';
 import { ShowType } from '../../../types/Media';
+import { motion } from 'framer-motion';
 
 type PropTypes = {
   tmdb_data: TmdbType;
@@ -8,18 +9,57 @@ type PropTypes = {
 };
 
 export default function Episodes({ tmdb_data, media_data }: PropTypes) {
+  const overview_text =
+    media_data.type == 'tv'
+      ? tmdb_data[`season/${media_data.season}`].overview || tmdb_data.overview
+      : tmdb_data.overview;
+
   return (
-    <section className={styles.container}>
+    <motion.section
+      className={styles.container}
+      variants={{
+        visible: {
+          opacity: 1,
+          transition: {
+            delayChildren: 0.1 + 0.3 * (overview_text.length / 200),
+            staggerChildren: 0.2,
+          },
+        },
+        hidden: {
+          opacity: 0,
+        },
+      }}
+    >
       {tmdb_data[`season/${media_data.season}`].episodes
         .slice(media_data.epiStart - 1, media_data.epiEnd)
         .map((ele: any, idx: number) => {
           return (
-            <section className={styles.episode_container} key={idx}>
+            <motion.section
+              className={styles.episode_container}
+              key={idx}
+              variants={{
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    y: {
+                      duration: 0.15,
+                    },
+                  },
+                },
+                hidden: {
+                  opacity: 0,
+                  y: -100,
+                },
+              }}
+            >
               <div className={styles.still_wrapper}>
                 <img
                   className={styles.still}
                   src={`https://image.tmdb.org/t/p/w342${
-                    ele.still_path ? ele.still_path : tmdb_data[`season/${media_data.season}`].poster_path
+                    ele.still_path
+                      ? ele.still_path
+                      : tmdb_data[`season/${media_data.season}`].poster_path
                   }`}
                   alt={tmdb_data.original_name}
                 />
@@ -33,9 +73,9 @@ export default function Episodes({ tmdb_data, media_data }: PropTypes) {
                 </div>
                 <p className={styles.overview}>{ele.overview}</p>
               </div>
-            </section>
+            </motion.section>
           );
         })}
-    </section>
+    </motion.section>
   );
 }
