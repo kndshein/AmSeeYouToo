a3c9995ae8e7e9adadcb52137f674c5ce3b5fd90
kndshein <kndshein@gmail.com>
2024-06-17 00:19:10 -0700

    Undo relative path, change back to absolute pathing

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/assets-v3',
+  base: 'https://amseeyou-v3.netlify.app',
 });
